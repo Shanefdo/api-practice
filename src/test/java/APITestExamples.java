fb997ef592c10235a040cd3bda873192fc62272d
shanefdo <$2023@Git%>
2026-05-21 15:49:56 +0100

    Add test 2 and 3 changes

--- src/test/java/APITestExamples.java
+++ src/test/java/APITestExamples.java
@@ -2,11 +2,14 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
+
 public class APITestExamples {
 
     @Test
-    public void test_1(){
-       Response response = RestAssured.get("https://courseenrollmentapimanagementsystem.onrender.com/status");
+    public void test_1() {
+        Response response = RestAssured.get("https://courseenrollmentapimanagementsystem.onrender.com/status");
 
 //        System.out.println(response.statusCode());
 //        System.out.println(response.body().asString());
@@ -16,5 +19,26 @@ public class APITestExamples {
         int statusCode = response.statusCode();
 
         assert statusCode == 200 : "Expected status code 200 but got " + statusCode;
+    }
+
+
+    @Test
+    public void test_2() {
+        baseURI = "https://courseenrollmentapimanagementsystem.onrender.com";
+
+        given().get("/status")
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("Server is running"));
+    }
+
+    @Test
+    public void test_3(){
+        baseURI = "https://courseenrollmentapimanagementsystem.onrender.com";
+
+        given().get("/courses/all")
+                .then()
+                .statusCode(200)
+                .body("_id[5]", equalTo("69d4cd4b51d014471d4162ec"));
     }
 }
